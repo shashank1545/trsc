ae4a33a6f03cb16a7ac052488b7a5ab7ef8e069b
shashank1545 <shashank1545@gmail.com>
2026-02-11 18:08:39 +0530

    Implement command-line parsing
    It is responsible for pasing command line flags (-dump-token, -dump-ast,
    dump-symbol, -v/--verbose and others..) , validating the output and
    input files and setting the CompilerOptions .

---
+++ include/trsc/Basic/CommandLine.h
@@ -0,0 +1,22 @@
+#ifndef TRSC_BASIC_COMMANDLINE_H
+#define TRSC_BASIC_COMMANDLINE_H
+
+#include <string>
+
+namespace trsc {
+
+struct CompilerOptions {
+  std::string InputFile;
+  std::string OutputFile;
+  bool DumpLexerTokens = false;
+  bool DumpAST = false;
+  bool Verbose = false;
+  bool DumpSymbol = false;
+  bool DumpSymbolTable = false;
+};
+
+bool parseCommandLine(int Argc, char **Argv, CompilerOptions &Options);
+
+} // namespace trsc
+
+#endif // TRSC_BASIC_COMMANDLINE_H
---
+++ lib/Basic/CommandLine.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+
+#include "trsc/Basic/CommandLine.h"
+
+namespace trsc {
+
+bool parseCommandLine(int argc, char **argv, CompilerOptions &options) {
+  for (int i = 1; i < argc; ++i) {
+    std::string arg = argv[i];
+    if (arg == "-dump-token") {
+      options.DumpLexerTokens = true;
+    } else if (arg == "-dump-ast") {
+      options.DumpAST = true;
+    } else if (arg == "-v" || arg == "--verbose") {
+      options.Verbose = true;
+    } else if (arg == "-dump-symbol") {
+      options.DumpSymbol = true;
+    } else if (arg == "-dump-symboltable") {
+      options.DumpSymbolTable = true;
+    } else if (arg == "-o") {
+      if (i + 1 < argc) {
+        options.OutputFile = argv[++i];
+      } else {
+        std::cerr << "Error: -o requires a filename." << '\n';
+        return false;
+      }
+    } else {
+      if (!options.InputFile.empty()) {
+        std::cerr << "Error: Only one input file can be specified." << '\n';
+        return false;
+      }
+      options.InputFile = arg;
+    }
+  }
+
+  if (options.InputFile.empty()) {
+    std::cerr << "Error: No input file specified." << '\n';
+    return false;
+  }
+
+  return true;
+}
+
+} // namespace trsc
+
