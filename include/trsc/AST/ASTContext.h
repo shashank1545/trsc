51cdddc82e1a833416a207327dc14fc3192a1ca3
shashank1545 <shashank1545@gmail.com>
2026-02-27 23:43:15 +0530

    AST Context implementation for QualType operations

---
+++ include/trsc/AST/ASTContext.h
@@ -0,0 +1,92 @@
+#ifndef TRSC_AST_ASTCONTEXT_H
+#define TRSC_AST_ASTCONTEXT_H
+
+#include <memory>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include "trsc/AST/QualType.h"
+
+namespace trsc {
+
+class ASTContext {
+  private:
+    std::unique_ptr<U8BuiltinType> U8Ty;
+    std::unique_ptr<U16BuiltinType> U16Ty;
+    std::unique_ptr<U32BuiltinType> U32Ty;
+    std::unique_ptr<U64BuiltinType> U64Ty;
+    std::unique_ptr<U128BuiltinType> U128Ty;
+    std::unique_ptr<USizeBuiltinType> USizeTy;
+
+    std::unique_ptr<I8BuiltinType> I8Ty;
+    std::unique_ptr<I16BuiltinType> I16Ty;
+    std::unique_ptr<I32BuiltinType> I32Ty;
+    std::unique_ptr<I64BuiltinType> I64Ty;
+    std::unique_ptr<I128BuiltinType> I128Ty;
+    std::unique_ptr<ISizeBuiltinType> ISizeTy;
+
+    std::unique_ptr<F32BuiltinType> F32Ty;
+    std::unique_ptr<F64BuiltinType> F64Ty; 
+
+    std::unique_ptr<BoolBuiltinType> BoolTy;
+
+    std::unique_ptr<CharBuiltinType> CharTy;
+
+    std::unique_ptr<StringBuiltinType> StringTy; 
+
+    std::unique_ptr<UnitType> UnitTy;
+    
+    std::unordered_map<std::string, QualType> TypeMap;
+
+    std::unordered_map<PointerTypeKey, std::unique_ptr<PointerType>, 
+      PointerTypeKeyHasher> PtrTy;
+    std::unordered_map<ReferenceTypeKey, std::unique_ptr<ReferenceType>, 
+      ReferenceTypeKeyHasher> RefTy;
+    std::unordered_map<FunctionTypeKey, std::unique_ptr<FunctionType>, 
+      FunctionTypeKeyHasher> FuncTy;
+
+    void initializeBuiltinTypes();
+
+  public:
+    ASTContext();
+
+    QualType getU8Type() const { return QualType(U8Ty.get()); }
+    QualType getU16Type() const { return QualType(U16Ty.get()); }
+    QualType getU32Type() const { return QualType(U32Ty.get()); }
+    QualType getU64Type() const { return QualType(U64Ty.get()); }
+    QualType getU128Type() const { return QualType(U128Ty.get()); }
+    QualType getUSizeType() const { return QualType(USizeTy.get()); }
+
+    QualType getI8Type() const { return QualType(I8Ty.get()); }
+    QualType getI16Type() const { return QualType(I16Ty.get()); }
+    QualType getI32Type() const { return QualType(I32Ty.get()); }
+    QualType getI64Type() const { return QualType(I64Ty.get()); }
+    QualType getI128Type() const { return QualType(I128Ty.get()); }
+    QualType getISizeType() const { return QualType(ISizeTy.get()); }
+
+    QualType getF32Type() const { return QualType(F32Ty.get()); }
+    QualType getF64Type() const { return QualType(F64Ty.get()); }
+
+    QualType getBoolType() const { return QualType(BoolTy.get()); }
+
+    QualType getCharType() const { return QualType(CharTy.get());}
+
+    QualType getStringType() const { return QualType(StringTy.get());}
+
+    QualType getUnitType() const { return QualType(UnitTy.get());}
+
+    QualType getPointerType(QualType PointeeType, bool IsMutable); 
+    QualType getReferenceType(QualType ReferentType, bool IsMutable);
+    QualType getFunctionType(QualType ReturnType, 
+        const std::vector<QualType>& ParamsType); 
+
+    QualType getTypeByName(const std::string& Name) const;
+
+    bool areTypesCompatible(QualType T1, QualType T2) const;
+
+    bool canImplicitlyConvert(QualType From, QualType To) const;
+};
+
+} // namespace trsc
+
+#endif // TRSC_AST_ASTCONTEXT_H
---
+++ lib/AST/ASTContext.cpp
@@ -0,0 +1,137 @@
+
+#include "trsc/AST/ASTContext.h"
+#include "trsc/AST/QualType.h"
+#include <memory>
+
+namespace trsc {
+
+ASTContext::ASTContext() { initializeBuiltinTypes(); }
+
+void ASTContext::initializeBuiltinTypes() {
+  U8Ty = std::make_unique<U8BuiltinType>();
+  TypeMap[U8Ty->getName()] = QualType(U8Ty.get());
+
+  U16Ty = std::make_unique<U16BuiltinType>();
+  TypeMap[U16Ty->getName()] = QualType(U16Ty.get());
+
+  U32Ty = std::make_unique<U32BuiltinType>();
+  TypeMap[U32Ty->getName()] = QualType(U32Ty.get());
+
+  U64Ty = std::make_unique<U64BuiltinType>();
+  TypeMap[U64Ty->getName()] = QualType(U64Ty.get());
+
+  U128Ty = std::make_unique<U128BuiltinType>();
+  TypeMap[U128Ty->getName()] = QualType(U128Ty.get());
+
+  USizeTy = std::make_unique<USizeBuiltinType>();
+  TypeMap[USizeTy->getName()] = QualType(USizeTy.get());
+
+  I8Ty = std::make_unique<I8BuiltinType>();
+  TypeMap[I8Ty->getName()] = QualType(I8Ty.get());
+
+  I16Ty = std::make_unique<I16BuiltinType>();
+  TypeMap[I16Ty->getName()] = QualType(I16Ty.get());
+
+  I32Ty = std::make_unique<I32BuiltinType>();
+  TypeMap[I32Ty->getName()] = QualType(I32Ty.get());
+
+  I64Ty = std::make_unique<I64BuiltinType>();
+  TypeMap[I64Ty->getName()] = QualType(I64Ty.get());
+
+  I128Ty = std::make_unique<I128BuiltinType>();
+  TypeMap[I128Ty->getName()] = QualType(I128Ty.get());
+
+  ISizeTy = std::make_unique<ISizeBuiltinType>();
+  TypeMap[ISizeTy->getName()] = QualType(ISizeTy.get());
+
+  F32Ty = std::make_unique<F32BuiltinType>();
+  TypeMap[F32Ty->getName()] = QualType(F32Ty.get());
+
+  F64Ty = std::make_unique<F64BuiltinType>();
+  TypeMap[F64Ty->getName()] = QualType(F64Ty.get());
+
+  BoolTy = std::make_unique<BoolBuiltinType>();
+  TypeMap[BoolTy->getName()] = QualType(BoolTy.get());
+
+  CharTy = std::make_unique<CharBuiltinType>();
+  TypeMap[CharTy->getName()] = QualType(CharTy.get());
+
+  StringTy = std::make_unique<StringBuiltinType>();
+  TypeMap[StringTy->getName()] = QualType(StringTy.get());
+
+  UnitTy = std::make_unique<UnitType>();
+  TypeMap[UnitTy->getName()] = QualType(UnitTy.get());
+}
+
+QualType ASTContext::getTypeByName(const std::string &Name) const {
+  auto it = TypeMap.find(Name);
+  if (it != TypeMap.end()) {
+    return it->second;
+  }
+  return QualType();
+}
+
+QualType ASTContext::getPointerType(QualType PointeeType, bool IsMutable) {
+  PointerTypeKey Key = {PointeeType, IsMutable};
+  auto It = PtrTy.find(Key);
+  if (It != PtrTy.end()) {
+    return QualType(It->second.get());
+  }
+  PtrTy[Key] = std::make_unique<PointerType>(PointeeType, IsMutable);
+  return QualType(PtrTy[Key].get());
+}
+
+QualType ASTContext::getReferenceType(QualType ReferentType, bool IsMutable) {
+  ReferenceTypeKey Key = {ReferentType, IsMutable};
+  auto It = RefTy.find(Key);
+  if (It != RefTy.end()) {
+    return QualType(It->second.get());
+  }
+  RefTy[Key] = std::make_unique<ReferenceType>(ReferentType, IsMutable);
+  return QualType(RefTy[Key].get());
+}
+
+QualType ASTContext::getFunctionType(QualType ReturnType, 
+    const std::vector<QualType>& ParamsType) {
+  FunctionTypeKey Key = {ReturnType, ParamsType};
+  auto It = FuncTy.find(Key);
+  if (It != FuncTy.end()) {
+    return QualType(It->second.get());
+  }
+  FuncTy[Key] = std::make_unique<FunctionType>(ReturnType, ParamsType);
+  return QualType(FuncTy[Key].get());
+}
+
+bool ASTContext::areTypesCompatible(QualType T1, QualType T2) const {
+    if (T1.isNull() || T2.isNull()) {
+        return false;
+    }
+    return T1.getTypePtr() == T2.getTypePtr();
+}
+
+bool ASTContext::canImplicitlyConvert(QualType From, QualType To) const {
+    if (areTypesCompatible(From, To)) {
+        return true;
+    }
+
+    if (From.isIntegerType() && To.isIntegerType()) {
+        return true;
+    }
+
+    if (From.isFloatingType() && To.isFloatingType()) {
+        return true;
+    }
+
+    if (From.isIntegerType() && To.isFloatingType()) {
+        return true;
+    }
+
+    if (From.isBooleanType() && To.isBooleanType()) {
+        return true;
+    }
+
+    return false;
+}
+
+} // namespace trsc
+
