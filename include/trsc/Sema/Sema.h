31e1a2da69fad614fc3c1dccc18397cbad09e351
shashank1545 <shashank1545@gmail.com>
2026-03-01 18:58:09 +0530

    The Sema phase implementation .
    The semantic analysis phase starts and ends here.

---
+++ include/trsc/Sema/Sema.h
@@ -0,0 +1,38 @@
+#ifndef TRSC_SEMA_SEMA_H
+#define TRSC_SEMA_SEMA_H
+
+#include "trsc/AST/AST.h"
+#include "trsc/AST/ASTContext.h"
+#include "trsc/Basic/Diagnostics.h"
+#include "trsc/Sema/BorrowChecker.h"
+#include "trsc/Sema/DeclarationCollector.h"
+#include "trsc/Sema/NameResolver.h"
+#include "trsc/Sema/SymbolTable.h"
+#include "trsc/Sema/TypeChecker.h"
+
+namespace trsc {
+
+class SemanticAnalyzer {
+public:
+  SemanticAnalyzer(DiagnosticsEngine &Diags); 
+  ~SemanticAnalyzer(); 
+
+  void analyze(ASTNode *Ast);
+
+  SymbolTable& getSymbolTable() { return ST; }
+  ASTContext& getASTContext() { return Ctx; }
+  DiagnosticsEngine& getDiagnostics() { return Diags; }
+
+private:
+  DiagnosticsEngine &Diags;
+  SymbolTable ST;
+  ASTContext Ctx;
+  DeclarationCollector DeclarationCollector;
+  NameResolver NameResolver;
+  TypeChecker TypeChecker;
+  BorrowChecker BorrowChecker;
+};
+
+} // namespace trsc
+
+#endif // TRSC_SEMA_SEMA_H
---
+++ lib/Sema/Sema.cpp
@@ -0,0 +1,22 @@
+#include "trsc/Sema/Sema.h"
+#include "trsc/Sema/DeclarationCollector.h"
+#include "trsc/Sema/TypeChecker.h"
+#include "trsc/Sema/BorrowChecker.h" 
+
+namespace trsc {
+
+  SemanticAnalyzer::SemanticAnalyzer(DiagnosticsEngine &Diags)
+    : Diags(Diags), ST(), Ctx(), DeclarationCollector(Diags, ST), 
+    NameResolver(Diags, ST), TypeChecker(Diags, ST, Ctx), 
+    BorrowChecker(Diags, ST, Ctx) {}
+
+  SemanticAnalyzer::~SemanticAnalyzer() = default; 
+
+  void SemanticAnalyzer::analyze(ASTNode *Ast) {
+    DeclarationCollector.visit(Ast);
+    NameResolver.visit(Ast);
+    TypeChecker.visit(Ast);
+    BorrowChecker.visit(Ast);
+  }
+
+} // namespace trsc
