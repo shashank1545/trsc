af349a2aad6539e236dcb1d380b06441b42d39f5
shashank1545 <shashank1545@gmail.com>
2026-03-05 15:52:23 +0530

    Removed unnecesary includes and corrected the indentation

--- lib/Sema/Sema.cpp
+++ lib/Sema/Sema.cpp
@@ -1,25 +1,19 @@
 #include "trsc/Sema/Sema.h"
-#include "trsc/AST/ASTContext.h"
-#include "trsc/Sema/DeclarationCollector.h"
-#include "trsc/Sema/SymbolTable.h"
-#include "trsc/Sema/TypeChecker.h"
-#include "trsc/Sema/BorrowChecker.h" 
 
-namespace trsc {
+using namespace trsc; 
 
-  SemanticAnalyzer::SemanticAnalyzer(DiagnosticsEngine &Diags, SymbolTable &ST, 
-      ASTContext &Ctx) : Diags(Diags), ST(ST), Ctx(Ctx), 
+SemanticAnalyzer::SemanticAnalyzer(DiagnosticsEngine &Diags, SymbolTable &ST, 
+    ASTContext &Ctx) : Diags(Diags), ST(ST), Ctx(Ctx), 
   DeclarationCollector(Diags, ST), 
   NameResolver(Diags, ST), TypeChecker(Diags, ST, Ctx), 
   BorrowChecker(Diags, ST, Ctx) {}
 
-  SemanticAnalyzer::~SemanticAnalyzer() = default; 
+SemanticAnalyzer::~SemanticAnalyzer() = default; 
 
-  void SemanticAnalyzer::analyze(ASTNode *Ast) {
-    DeclarationCollector.visit(Ast);
-    NameResolver.visit(Ast);
-    TypeChecker.visit(Ast);
-    BorrowChecker.visit(Ast);
-  }
+void SemanticAnalyzer::analyze(ASTNode *Ast) {
+  DeclarationCollector.visit(Ast);
+  NameResolver.visit(Ast);
+  TypeChecker.visit(Ast);
+  BorrowChecker.visit(Ast);
+}
 
-} // namespace trsc
