943bcb04885ebda669341b8f721ee1e797469066
shashank1545 <shashank1545@gmail.com>
2026-03-09 17:54:10 +0530

    Naming changes, used OpTy::create instead of the deprecated builder::create

--- lib/MLIR/MLIRGen/MLIRGen.cpp
+++ lib/MLIR/MLIRGen/MLIRGen.cpp
@@ -1,7 +1,12 @@
 
+#include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/Dialect/MemRef/IR/MemRef.h"
+#include "mlir/IR/BuiltinOps.h"
+#include "mlir/IR/BuiltinTypeInterfaces.h"
 #include "mlir/IR/Types.h"
+#include "mlir/IR/Value.h"
+#include "mlir/Support/LLVM.h"
 #include "trsc/AST/AST.h"
-#include "trsc/AST/ASTVisitor.h"
 #include "trsc/AST/QualType.h"
 #include "trsc/AST/ASTContext.h"
 #include "trsc/MLIR/TrscMLIRGen.h"
@@ -12,6 +17,9 @@
 #include "mlir/IR/OwningOpRef.h"
 #include "mlir/IR/Verifier.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/ControlFlow/IR/ControlFlow.h"
+#include "mlir/Dialect/MemRef/IR/MemRef.h"
+#include "llvm/Support/Casting.h"
 #include <vector>
 
 using namespace trsc; 
@@ -28,7 +36,7 @@ MLIRGen::MLIRGen(mlir::MLIRContext &MLIRCtx, trsc::ASTContext &ASTCtx,
     MLIRCtx.loadAllAvailableDialects();
   }
 
-mlir::Type MLIRGen::convertType(QualType T) {
+mlir::Type MLIRGen::ToMLIRType(QualType T) {
   if (T.isNull() || T.isUnitType()) {
     return Builder.getNoneType();
   }
@@ -44,17 +52,29 @@ mlir::Type MLIRGen::convertType(QualType T) {
   return Builder.getNoneType();
 }
 
-void MLIRGen::visitProgram(Program *Node) {
-  ASTVisitor<MLIRGen>::visitProgram(Node);
+mlir::MemRefType MLIRGen::ToMemRefType(QualType T) {
+    return mlir::MemRefType::get({}, ToMLIRType(T));
+  }
+
+llvm::APFloat MLIRGen::ToAPFloat(double D) {
+  return llvm::APFloat(D); 
 }
 
-void MLIRGen::visitBlockStmt(BlockStmt *Node) {
-  for (const auto &Stmt : Node->getStatements()) {
-    visit(Stmt.get());
   }
 }
 
-void MLIRGen::visitFuncDecl(FuncDecl *Node) {
+void MLIRGen::genProgram(Program *Node) {
+  for(auto& S: Node->getStatements()) {
+    genStmt(S.get());
+  }
+}
+
+void MLIRGen::genBlockStmt(BlockStmt *Node) {
+  for (const auto &S : Node->getStatements()) {
+    genStmt(S.get());
+  }
+}
+
   mlir::memref::AllocaOp VarAlloca;
   {
     mlir::OpBuilder::InsertionGuard Guard(Builder);
@@ -64,6 +84,7 @@ void MLIRGen::visitFuncDecl(FuncDecl *Node) {
         ToMemRefType(VarTy));  
   }
   Sym->setAlloca(VarAlloca);
+void MLIRGen::genFuncDecl(FuncDecl *Node) {
   Symbol* Sym = ST.lookupSymbol(Node->getFuncName()->getName());
   if (!Sym) return;
 
@@ -72,45 +93,51 @@ void MLIRGen::visitFuncDecl(FuncDecl *Node) {
 
   std::vector<mlir::Type> ArgTypes;
   for (const auto& ParamTy : FuncTy.getParamsType()) {
-    ArgTypes.push_back(convertType(ParamTy));
+    ArgTypes.push_back(ToMLIRType(ParamTy));
   }
   
   QualType RetT = FuncTy.getReturnType();
   std::vector<mlir::Type> ResultTypes;
-  if (!RetT.isUnitType() && !RetT.isNull()) {
-    ResultTypes.push_back(convertType(RetT));
+  if (!RetT.isUnitType()) {
+    ResultTypes.push_back(ToMLIRType(RetT));
   }
 
   auto FuncType = Builder.getFunctionType(ArgTypes, ResultTypes);
 
-  auto FuncOp = Builder.create<mlir::func::FuncOp>(
+  auto FuncOp = mlir::func::FuncOp::create(
+      Builder,
       Builder.getUnknownLoc(),
       Node->getFuncName()->getName(),
       FuncType
       );
 
   auto *EntryBlock = FuncOp.addEntryBlock();
+  this->CurrentEntryBlock = EntryBlock;
 
   mlir::OpBuilder::InsertionGuard Guard(Builder);
-  Builder.setInsertionPointToStart(EntryBlock);
+  Builder.setInsertionPointToStart(CurrentEntryBlock);
 
   if(Node->getBody()) {
-    visit(Node->getBody());
+    genStmt(Node->getBody());
   }
 
   if (ResultTypes.empty()) {
     if (EntryBlock->empty() || 
         !EntryBlock->back().hasTrait<mlir::OpTrait::IsTerminator>()) {
-      Builder.create<mlir::func::ReturnOp>(Builder.getUnknownLoc());
+      mlir::func::ReturnOp::create(Builder, Builder.getUnknownLoc());
     }
   }
+
+  this->CurrentEntryBlock = nullptr;
 }
 
-mlir::OwningOpRef<mlir::ModuleOp> MLIRGen::generate(trsc::Program &Prog) {
+}
+
+mlir::OwningOpRef<mlir::ModuleOp> MLIRGen::genModule(trsc::Program &Prog) {
   Module = mlir::ModuleOp::create(mlir::UnknownLoc::get(&MLIRCtx));
   Builder.setInsertionPointToEnd(Module.getBody());
   
-  visitProgram(&Prog);
+  genProgram(&Prog);
 
   if (failed(mlir::verify(Module))) {
     Module.emitError("Module verification failed.");
